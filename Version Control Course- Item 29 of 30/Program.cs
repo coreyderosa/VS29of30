5718f4a19830d2274c1c146af086393b80baab47
coreyderosa <coreyderosa@gmail.com>
2016-07-02 17:25:25 -0600

    deleted file system stuff in project at top of code

--- Version Control Course- Item 29 of 30/Program.cs
+++ Version Control Course- Item 29 of 30/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Version_Control_Course__Item_29_of_30
+﻿namespace Version_Control_Course__Item_29_of_30
 {
     class Program
     {
